c518992eb4009d7c79a3c791a43507b868f5da98
Gerdazijlstra <150154@csgaugustinus.nl>
2020-05-21 10:13:53 +0000

    fcodjwefj

--- public/mijnProject/index.php
+++ public/mijnProject/index.php
@@ -25,7 +25,6 @@
 <div id= "topnav"> 
 </div>
 <div id= "container"> 
-<<<<<<< HEAD
     <div class="colum left">
 
     </div>
@@ -33,12 +32,6 @@
     
     </div>
     
-=======
-<div class="colum left">
-<p> Hoi allemaal! bij bakken & co laten wij je verschillende recepten zien die je kan maken. Vergeet niet om een account aan te maken, zo kan je zelf ook recepten delen. Veel bakplezier!</p>
-</div>
-<div class="column right"> </div>
->>>>>>> cd8e8b7f52a7fdebc4f63903751f087bc8112d1f
 </div>
 <div id= "footer">
 
