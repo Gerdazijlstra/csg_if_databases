d470bb2b2dc45abde4906fce713b21805935fe7d
Diannedejong <150512@csgaugustinus.nl>
2020-05-21 09:52:26 +0000

    dw

--- public/mijnProject/index.php
+++ public/mijnProject/index.php
@@ -1,19 +1,4 @@
-<?php
-error_reporting(E_ALL & ~E_NOTICE);
-require('php/database.php');
 
-//maak databaseverbinding met de gegevens uit database.php
-$DBverbinding = mysqli_connect($servernaam, $gebruikersnaam, $wachtwoord, $database);
-// Controleer de verbinding
-if (!$DBverbinding) {
-// Geef de foutmelding die de server teruggeeft en stop met de uitvoer van PHP (die)
-die("Verbinding mislukt: " . mysqli_connect_error());
-}
-else {
-// Dit gedeelte laat je normaliter weg, maar is hier ter illustratie toegevoegd
-echo '<i>verbinding database succesvol</i>';
-}
-?>
 <!DOCTYPE html>
 <html>
     <head>
