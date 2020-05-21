72e55326fa8ea4ed0ca192b02ce39315e6247716
Diannedejong <150512@csgaugustinus.nl>
2020-05-21 10:15:11 +0000

    dwedw

--- public/mijnProject/index.php
+++ public/mijnProject/index.php
@@ -8,36 +8,30 @@
     </head>
     <body>
         <div id= "header"> 
-
-<div id="h1"> 
-
-</div>
-<div id="h2"> 
-<h1> bakken & co </h1>
-<br>
-<h2> bakken doe je zo!</h2>
-</div>
-<div id="h3"> </div>
-
-</div>
-<div id= "topnav"> 
-  <a href="home.html">home</a>
+            <div id="h1"> 
+            </div>
+            <div id="h2"> 
+                <h1> bakken & co </h1>
+                <br>
+                <h2> bakken doe je zo!</h2>
+            </div>
+            <div id="h3"> 
+            </div>
+        </div>
+        <div id= "topnav"> 
+            <a href="home.html">home</a>
             <a href="waar.html">locatie & tarieven</a>
             <a href="over_de_camping.html">over de camping</a>
             <a href="contact.html">contact</a>
             <a href="colofon.html">colofon</a>
-</div>
-<div id= "container"> 
-    <div class="colum left">
-
-    </div>
-    <div class="column right"> 
-    
-    </div>
-    
-</div>
-<div id= "footer">
-
-</div>
-</body>
+        </div>
+        <div id= "container"> 
+            <div class="colum left">
+            </div>
+            <div class="column right"> 
+            </div>
+        </div>
+        <div id= "footer">
+        </div>
+    </body>
 </html>
