f2f744f801da6bd68926900d61f233e09411ff92
diannedejong1 <dianne.dejong@home.nl>
2020-05-22 00:26:00 +0000

    m

--- public/mijnProject/index.php
+++ public/mijnProject/index.php
@@ -17,17 +17,25 @@
             <div id="h3"> 
             </div>
         </div>
-        <div id= "topnav"> 
+        <div id= "menu"> 
             <a href="index.php">home</a>
             <a href="recepten.php">recepten</a>
             <a href="inloggen.php">inloggen</a>
         </div>
-        <div id= "container"> 
-            <div class="colum left">
-                <h1>a</h1>
+        <div id= "midden"> 
+            <div id="lings">
+                <div class="een"></div>
+                <div class="twee"></div>
+                <div class="drie"></div>
+                <div class="vier"></div>
+                <div class="vijf"></div>
+                <div class="zes"></div>
+                <div class="zeven"></div>
+                <div class="acht"></div>
+                <div class="negen"></div>
+                             
             </div>
-            <div class="column right"> 
-                <h1>b</h1>
+            <div id="rechts"> 
             </div>
         </div>
         <div id= "footer">
