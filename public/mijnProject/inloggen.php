f7418116e766c9bc0117d152eb9b5dc425cdfa48
diannedejong1 <dianne.dejong@home.nl>
2020-05-26 08:46:09 +0000

    ewf

---
+++ public/mijnProject/inloggen.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Bakken & co</title>
+        <link rel="stylesheet" type="text/css" href="css/design.css">
+        <title>recepten</title><link rel="stylesheet" href="design.css" type="text/css">
+    </head>
+    <body>
+        <div id= "header"> 
+            <div id="h1"> 
+            </div>
+            <div id="h2"> 
+                <h1> bakken & co </h1>
+                <h3> bakken doe je zo!</h3>
+            </div>
+            <div id="h3"> 
+            </div>
+        </div>
+        <div id= "menu"> 
+            <a href="index.php">home</a>
+            <a href="recepten.php">recepten</a>
+            <a href="inloggen.php">inloggen</a>
+        </div>
+        <div id= "midden"> 
+
+                             
+
+         
+        </div>
+        <div id= "footer">
+        </div>
+    </body>
+</html>
---
+++ public/mijnProject/recepten.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Bakken & co</title>
+        <link rel="stylesheet" type="text/css" href="css/design.css">
+        <title>recepten</title><link rel="stylesheet" href="design.css" type="text/css">
+    </head>
+    <body>
+        <div id= "header"> 
+            <div id="h1"> 
+            </div>
+            <div id="h2"> 
+                <h1> bakken & co </h1>
+                <h3> bakken doe je zo!</h3>
+            </div>
+            <div id="h3"> 
+            </div>
+        </div>
+        <div id= "menu"> 
+            <a href="index.php">home</a>
+            <a href="recepten.php">recepten</a>
+            <a href="inloggen.php">inloggen</a>
+        </div>
+        <div id= "midden"> 
+            <div id="lings">
+                <div id= "a">
+                    <div class="een"></div>
+                    <div class="twee"></div>
+                    <div class="drie"></div>
+                </div>
+                <div id="b">
+                    <div class="vier"></div>
+                    <div class="vijf"></div>
+                    <div class="zes"></div>
+                </div>
+                <div id="c">
+                    <div class="zeven"></div>
+                    <div class="acht"></div>
+                    <div class="negen"></div>
+                </div>
+                             
+            </div>
+            <div id="rechts"> 
+            </div>
+        </div>
+        <div id= "footer">
+        </div>
+    </body>
+</html>
