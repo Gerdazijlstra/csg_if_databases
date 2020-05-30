c0da19c66f67d692a912758413c3ca94df899fc6
Gerdazijlstra <150154@csgaugustinus.nl>
2020-05-30 15:52:16 +0000

    dsasce

--- public/mijnProject/inloggen.php
+++ public/mijnProject/inloggen.php
@@ -18,39 +18,7 @@
                 <a href="inloggen.php">inloggen</a>
             </div>
             <div id= "midden"> 
-                <?php
-                include('db.php');
-                    if (isset($_SESSION['Inloggen'])){
-                        echo 'Welkom op Bakken & co!';
-                    }
-                    else{
-                       
-                        if(isset($_POST['submit'])){
-                            $sqlUitlezen= mysqli_query($MySQL, "SELECT * FROM 'gerbruikers");
-                            $sqlAantal= mysqli_num_rows($sqlUitlezen);
-                            if($sqlAantal > 0){
-                                while ($sqlData= mysqli_fetch_assoc($sqlUitlezen)){
-                                }
-                            }
-                        }
-                    }
-                ?>
-                <form> 
-                    <table width="100%" border="0" cellspacing="5" cellpadding="0" >
-                        <tr>
-                            <td width="100">Gebruikersnaam:</td>
-                            <td><input type="text" name="User" size="15" required="required" />
-                        </tr>
-                        <tr>
-                            <td>Wachtwoord:</td>
-                            <td><input type="password" name="Pass" size="15" required="required" />
-                        </tr>
-                        <tr>
-                            <td>&nbsp;</td>
-                            <td><input type="submit" name="submit" value="Inloggen" /></td>
-                        </tr>
-                    </table>
-                </form>                                       
+                                            
            </div>
             <div id= "footer">
             </div>
