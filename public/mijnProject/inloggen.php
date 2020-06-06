fff11b9fed1d550888d64735582f4bdd81769faf
Gerdazijlstra <150154@csgaugustinus.nl>
2020-06-06 13:51:03 +0000

    zcscsa

--- public/mijnProject/inloggen.php
+++ public/mijnProject/inloggen.php
@@ -8,13 +8,13 @@
     <form method="post" action="validate_login.php" >
         <table >
             <tr>
-                <td><label for="users_email">Email</label></td>
+                <td><label for="users_email">Gebruikersnaam</label></td>
                 <td><input type="text" 
 
                   name="users_email" id="users_email"></td>
             </tr>
             <tr>
-                <td><label for="users_pass">Password</label></td>
+                <td><label for="users_pass">Wachtwoord</label></td>
                 <td><input name="users_pass" 
 
                   type="password" id="users_pass"></input></td>
