260a0a23a478000d24362132154fd15065aa1cac
Gerdazijlstra <150154@csgaugustinus.nl>
2020-06-07 12:18:20 +0000

    sasaxq

--- public/mijnProject/inloggen.php
+++ public/mijnProject/inloggen.php
@@ -33,6 +33,7 @@ if (isset($_SESSION['Inloggen'])){
                 $sqlData = mysqli_fetch_assoc($sqlUitlezen);
                 
                 $_SESSION['Inloggen'] = $sqlData['Gebruikers'];
+                echo 'het werkt'
             }else{
                 echo 'Sorry, deze gegevens ken ik niet';
             }
