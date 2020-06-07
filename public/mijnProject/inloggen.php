c48d45becce0909d910235df921d0ddda5baac6a
Gerdazijlstra <150154@csgaugustinus.nl>
2020-06-07 12:56:16 +0000

    saewcew

--- public/mijnProject/inloggen.php
+++ public/mijnProject/inloggen.php
@@ -60,6 +60,7 @@ if (isset($_SESSION['Inloggen'])){
                   
            </div>
             <div id= "footer">
+            <p>&copy; 2020 - Website van Dianne en Gerda</p>
             </div>
     </body>
 </html>
