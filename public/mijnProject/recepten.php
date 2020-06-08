1b1c52e3849126cf2f00579f91be609f1fe01848
diannedejong1 <dianne.dejong@home.nl>
2020-06-08 08:28:43 +0000

    fdes

--- public/mijnProject/recepten.php
+++ public/mijnProject/recepten.php
@@ -23,14 +23,14 @@
                 <hr>
                 <div id= "a">
                     <div class="een">
-                        <img src="cake.jpg" alt="logo" height="80%" style="margin: auto" >
+                        <img src="cake.jpg" alt="recept1" height="80%" style="margin: auto" >
                         <br><br>
-                        <a href="index.php">klik hier</a>
+                        <a href="recept1.php">klik hier</a>
                     </div>
                     <div class="twee">
-                        <img src="cake.jpg" alt="logo" height="80%" style="margin: auto" >  
+                        <img src="cake.jpg" alt="recept2" height="80%" style="margin: auto" >  
                         <br><br>
-                        <a href="index.php">klik hier</a>                  
+                        <a href="recept2.php">klik hier</a>                  
                     </div>
                 </div>
                 <hr>
@@ -38,14 +38,14 @@
                 <hr>
                 <div id="b">
                     <div class="drie">
-                        <img src="cake.jpg" alt="logo" height="80%" style="margin: auto" >
+                        <img src="cake.jpg" alt="recept3" height="80%" style="margin: auto" >
                         <br><br>
-                        <a href="index.php">klik hier</a>
+                        <a href="recept3.php">klik hier</a>
                     </div>
                     <div class="vier">
-                        <img src="cake.jpg" alt="logo" height="80%" style="margin: auto" >
+                        <img src="cake.jpg" alt="recept4" height="80%" style="margin: auto" >
                         <br><br>
-                        <a href="index.php">klik hier</a>
+                        <a href="recept4.php">klik hier</a>
                     </div>
                 </div> 
                 <hr>
@@ -53,14 +53,14 @@
                 <hr>   
                 <div id="c">
                     <div class="vijf">
-                        <img src="cake.jpg" alt="logo" height="80%" style="margin: auto" >
+                        <img src="cake.jpg" alt="recept5" height="80%" style="margin: auto" >
                         <br><br>
-                        <a href="index.php">klik hier</a>
+                        <a href="recept5.php">klik hier</a>
                     </div>
                     <div class="zes">
-                        <img src="cake.jpg" alt="logo" height="80%" style="margin: auto" >
+                        <img src="cake.jpg" alt="recept6" height="80%" style="margin: auto" >
                         <br><br>
-                        <a href="index.php">klik hier</a>
+                        <a href="recept6.php">klik hier</a>
                     </div>
                 </div>
                 <hr>
@@ -68,14 +68,14 @@
                 <hr>
                 <div id="d">
                     <div class="zeven">
-                        <img src="cake.jpg" alt="logo" height="80%" style="margin: auto" >
+                        <img src="cake.jpg" alt="recept7" height="80%" style="margin: auto" >
                         <br><br>
-                        <a href="index.php">klik hier</a>
+                        <a href="recept7.php">klik hier</a>
                     </div>
                     <div class="acht">
-                        <img src="cake.jpg" alt="logo" height="80%" style="margin: auto" >
+                        <img src="cake.jpg" alt="recept8" height="80%" style="margin: auto" >
                         <br><br>
-                        <a href="index.php">klik hier</a>
+                        <a href="recept8.php">klik hier</a>
                     </div>
                     </div>
             </div>
