f2602afe20ace2dc4fb80cd2c0f9390a86500f19
diannedejong1 <dianne.dejong@home.nl>
2020-06-11 08:29:07 +0000

    cdsdv

--- public/mijnProject/recepten.php
+++ public/mijnProject/recepten.php
@@ -3,56 +3,54 @@
 require('php/begin.php');
 require('db.php');
 ?>
-                <hr>
                 <h1>zoet</h1>
                 <hr>
                 <div id= "a">
                     <div class="een">
-                        <img src="cake.jpg" alt="recept1" height="80%" style="margin: auto" >
+                        <img src="pics/zandkoekje.jpg" alt="recept1" height="80%" style="margin: auto" >
                         <br><br>
                         <a href="recept1.php">Zandkoekjes</a>
                     </div>
                     <div class="twee">
-                        <img src="cake.jpg" alt="recept2" height="80%" style="margin: auto" >  
+                        <img src="pics/appelcake.jpg" alt="recept2" height="80%" style="margin: auto" >  
                         <br><br>
                         <a href="recept2.php">Appelcake</a>                  
                     </div>
                 </div>
                 <div id="b">
                     <div class="drie">
-                        <img src="cake.jpg" alt="recept3" height="80%" style="margin: auto" >
+                        <img src="pics/tompoes.jpg" alt="recept3" height="80%" style="margin: auto" >
                         <br><br>
                         <a href="recept3.php">Aardbeientompoes</a>
                     </div>
                     <div class="vier">
-                        <img src="cake.jpg" alt="recept4" height="80%" style="margin: auto" >
+                        <img src="pics/choco.jpg" alt="recept4" height="80%" style="margin: auto" >
                         <br><br>
                         <a href="recept4.php">Chocoladetaart</a>
                     </div>
                 </div> 
-                <hr>
                 <h1>Hartig</h1>
                 <hr>   
                 <div id="c">
                     <div class="vijf">
-                        <img src="cake.jpg" alt="recept5" height="80%" style="margin: auto" >
+                        <img src="pics/broccoli.jpg" alt="recept5" height="80%" style="margin: auto" >
                         <br><br>
                         <a href="recept5.php">Broccoliquiche</a>
                     </div>
                     <div class="zes">
-                        <img src="cake.jpg" alt="recept6" height="80%" style="margin: auto" >
+                        <img src="pics/nachos.jpg" alt="recept6" height="80%" style="margin: auto" >
                         <br><br>
                         <a href="recept6.php">Nachos</a>
                     </div>
                 </div>
                 <div id="d">
                     <div class="zeven">
-                        <img src="cake.jpg" alt="recept7" height="80%" style="margin: auto" >
+                        <img src="pics/broodtaartje.jpg" alt="recept7" height="80%" style="margin: auto" >
                         <br><br>
                         <a href="recept7.php">Hartige broodtaartjes</a>
                     </div>
                     <div class="acht">
-                        <img src="cake.jpg" alt="recept8" height="80%" style="margin: auto" >
+                        <img src="pics/scones.jpg" alt="recept8" height="80%" style="margin: auto" >
                         <br><br>
                         <a href="recept8.php">Scones</a>
                     </div>
