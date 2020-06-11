f766b9a1ce393c94cc98ff484c0595d90e88cf51
Gerdazijlstra <150154@csgaugustinus.nl>
2020-06-11 17:37:29 +0000

    ascac

--- public/mijnProject/recept4.php
+++ public/mijnProject/recept4.php
@@ -1,5 +1,33 @@
 <?php
 require('php/begin.php');
+?>
+<div id= "lings">
+<h1>Chocoladetaart </h1>
+<br>
+<h3>benodigheden</h3>
+<ul>
+<li>1 mespunt zout</li>
+<li> 4 el cacaopoeder </li>
+<li> 225 g zelfrijzend bakmeel</li>
+<li> 200 g pure chocolade 72% </li>
+<li> 175 g witte basterdsuiker </li>
+<li> 100 g ongezouten roomboter </li>
+<li> 395 g gecondenseerde volle melk (blikje) </li>
+<li> 100 ml kraanwater (gekookt)</li>
+<li> 2 middelgrote eieren </li>
+<li> 150 g melkchocolade</li>
+<li> 250 ml verse slagroom </li>
+</ul>
+<h3> instructies </h3>
+<p> 1.	Verwarm de oven voor op 175 °C. Bekleed de bodem van de springvorm met bakpapier en vet de randen in met boter. Zeef het bakmeel, de cacao en het zout in een kom. Hak de pure chocolade grof. </p>
 
+<p>2.	Doe de suiker, boter, de helft van de pure chocolade, de gecondenseerde melk en kokend water in een pan. Verwarm al roerend op laag vuur tot een gladde massa. Voeg toe aan het bakmeel in de kom en roer tot een stevig geheel. Klop de eieren los, voeg ze toe en roer door. Schenk het mengsel in de springvorm en bak ca. 55 min. in het midden van de oven. </p>
+<p>3.	Hak ondertussen de melkchocolade fijn en doe het met de rest van de pure chocolade in een kom. Controleer de gaarheid door een satéprikker in het midden van de taart te steken. Is hij droog dan is de taart gaar. Verhit de slagroom tot tegen de kook aan, schenk in de kom en meng al roerend tot de chocolade gesmolten is. Klop de chocolade met de mixer tot een glanzende massa en laat 30 min. op kamertemperatuur iets opstijven.</p>
+<p>4.	Neem de taart uit de oven en laat 30 min. afkoelen in de vorm. Verwijder de vorm en laat in ca. 1.5 uur volledig afkoelen. Snijd de taart horizontaal doormidden. Bestrijk de bovenkant en beide helften met het chocolademengsel. Leg de helften weer op elkaar. </p>
+</div>
+<div id= "rechts">
+<img src="pics/chocotaart.png" alt="recept1" height="300px" style="margin: auto" >
+</div>
+<?php
 require('php/eind.php');
 ?>
