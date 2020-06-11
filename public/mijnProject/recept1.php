d89fa7e99e89093cafb4eece1ada99b4e480cb2f
diannedejong1 <dianne.dejong@home.nl>
2020-06-11 10:24:02 +0000

    cdascsd

--- public/mijnProject/recept1.php
+++ public/mijnProject/recept1.php
@@ -1,7 +1,7 @@
 <?php
 require('php/begin.php');
 ?>
-<img src="pics/zandkoekje.jpg" alt="recept1" height="80%" style="margin: auto" >
+<img src="pics/zandkoekje.jpg" alt="recept1" height="200px" style="margin: auto" >
 <h1>zandkoekjes </h1>
 <br>
 <h3>benodigheden</h3>
@@ -13,10 +13,9 @@ require('php/begin.php');
 </ul>
 <h3> instructies </h3>
 <p> 1.	Splits het ei en gebruik alleen de eidooier. Meng vervolgens alle ingrediënten in de keukenmachine tot het deeg een korrelige structuur heeft. Kneed de deegkruimels met koele handen tot een samenhangend deeg. Rol tot een bal en verpak in vershoudfolie en laat het 30 min. in de koelkast rusten. </p>
-<br>
+
 <p>2.	Verwarm de oven voor op 175 °C. Bestuif het werkblad met wat bloem en rol het deeg uit tot een lap van ca. 40 x 40 cm en 3-4 mm dik. Steek met behulp van uitsteekvormpjes of een glas koekjes uit het deeg. Kneed het resterende deeg tot een bal en rol opnieuw uit tot al het deeg gebruikt is. Leg de koekjes op enige afstand van elkaar op een met bakpapier beklede bakplaat. </p>
-<br>
-<p>
+<p>3.	Bak de koekjes in het midden van de oven in 15-20 min. lichtbruin en gaar. Neem ze van de bakplaat en laat afkoelen, bij voorkeur op een rooster.</p>
 
 <?php
 require('php/eind.php');
