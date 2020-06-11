138ed0cc697d9b0ca3938f8ffe7737ea1a6fcdab
diannedejong1 <dianne.dejong@home.nl>
2020-06-11 19:39:55 +0000

    cscs

--- public/mijnProject/inloggen1.php
+++ public/mijnProject/inloggen1.php
@@ -1,30 +1,36 @@
 <?php
-require('php/begin.php');
+    require('php/begin.php');
+    $naam = $_GET["gebruikersnaam"];
+    $woord = $_GET["wachtwoord"];
+    $ingelogd = 0 ;
+
+    if (empty($naam) && empty($woord)) {
+        echo 'log in';
+        }
+        else {
+            if ($naam == 'hoi'&& $woord == 'doei') {
+                echo 'u bent ingelogd';
+                $ingelogd = 1;
+            } else {
+                echo 'uw gebruikersnaam of wachtwoord is niet goed, probeer het nog eens';
+            }
+        }
+
+    if ($ingelogd== '1') {
+    
+}
+else {
 ?>
 <form action="inloggen1.php" method="get">
     gebruikersnaam: <input type="text" name="gebruikersnaam">
     <br>
-    wachtwoord: <input type="text" name="wachtwoord">
+    wachtwoord: <input type="password" name="wachtwoord">
     <br>
-    <input type="submit" >
+    <input type="submit" value="inloggen    " >
 </form>
 <br>
 
 <?php
-    $naam = $_GET["gebruikersnaam"];
-    $woord = $_GET["wachtwoord"];
-    $ingelogd ;
-
-    if (empty($naam) && empty($woord)) {
-     echo 'log in';
-    }
-    else {
-        if ($naam == 'hoi'&& $woord == 'doei') {
-            echo 'u bent ingelogd';
-        } else {
-            echo 'uw gebruikersnaam of wachtwoord is niet goed, probeer nog eens';
-        };
-    };
-
+}
     require('php/eind.php');
 ?>
