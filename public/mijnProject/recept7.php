4899359d2335c53a6476b59c6bbba5dee74d4f1a
Gerdazijlstra <150154@csgaugustinus.nl>
2020-06-11 17:48:23 +0000

    dascw

--- public/mijnProject/recept7.php
+++ public/mijnProject/recept7.php
@@ -1,5 +1,27 @@
 <?php
 require('php/begin.php');
+?>
+<div id= "lings">
+<h1>Hartige broodtaartjes </h1>
+<br>
+<h3>benodigheden</h3>
+<ul>
+<li>125 g magere spekreepjes</li>
+<li>2 middelgrote eieren </li>
+<li>2 el verse slagroom </li>
+<li>295 g gegrilde rode paprika's in pot</li>
+<li>4 afbakkaiserbroodjes</li>
+<li>70 g mozzarella</li>
+</ul>
+<h3> instructies </h3>
+<p> 1.	Verwarm de oven voor op 200 °C. Verhit een koekenpan zonder olie of boter en bak de spekreepjes op middelhoog vuur in 8 min. goudbruin. Laat uitlekken op keukenpapier. Laat de pan staan voor de volgende stap. </p>
 
+<p>2.	Klop de eieren met een garde los en klop de slagroom erdoor. Bak in het bakvet het ei-slagroommengsel al omscheppend met een spatel op middelhoog vuur in 5 min. net gaar. Snijd de paprika in reepjes en schep samen met het spek door het ei. Breng op smaak met peper en eventueel zout. </p>
+<p>3.	Snijd de kapjes van de broodjes. Hol de bolletjes uit en leg ze op een met bakpapier beklede bakplaat. Verdeel het eimengsel over de broodjes en druk aan. Snijd de mozzarella in blokjes, bestrooi er de broodjes mee en bak ca. 15 min. in het midden van de oven.</p>
+</div>
+<div id= "rechts">
+<img src="pics/broodtaart.png" alt="recept1" height="300px" style="margin: auto" >
+</div>
+<?php
 require('php/eind.php');
 ?>
