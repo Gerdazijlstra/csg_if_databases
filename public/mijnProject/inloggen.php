bf75e11ef8864b88aa480813f1ecb9cf05ae7320
Gerdazijlstra <150154@csgaugustinus.nl>
2020-06-11 08:43:31 +0000

    csweafd

--- public/mijnProject/inloggen.php
+++ public/mijnProject/inloggen.php
@@ -1,56 +1,26 @@
 <?php
 require('php/begin.php');
-require('db.php');
-$check = $check.'db.php geladen.<br>';
-$servernaam = "localhost";
-$Gebruikersnaam = 'username';
-$Wachtwoord = 'password';
-$host = 'localhost';
-$database = 'Bakkenenco';
-$DBverbinding = mysqli_connect($localhost, $Gebruikersnaam, $Wachtwoord, $Bakkenenco);
-$mysqli = new mysqli($localhost, $Gebruikersnaam, $Wachtwoord, $Bakkenenco);
-?> 
-               <?php
-                    if (isset($_SESSION['Inloggen'])){
-                        echo '<h1>Welkom op bakken & co!</h1>';
-                    }
-                    else{
-                        if (isset($_POST['submit'])){
-                                $sqlUitlezen = mysqli_query($MySQL, "SELECT * FROM `Gebruikers` WHERE `Gebruikersnaam` = '".$_POST['User']."' AND `Wachtwoord` = '".$_POST['Pass']."'");
-                               echo $sqlUitlezen;
-                                $sqlAantal = mysqli_num_rows($sqlUitlezen);
-                                        
-                                if ($sqlAantal == 1){
-                                    $sqlData = mysqli_fetch_assoc($sqlUitlezen);
-                                    
-                                    $_SESSION['Inloggen'] = $sqlData['Gebruikers'];
-                                }
-                                else{
-                                    echo 'Sorry, deze gegevens ken ik niet';
-                                }
-                        }   
-                echo '<PRE>'; 
-                print_r($_POST);
-                echo '</PRE>'; 
-                ?>
-                        <form method="post" action="#">
-                        <table width="100%" border="0" cellspacing="5" cellpadding="0">
-                        <tr>
-                        <td width="100">Gebruikersnaam:</td>
-                        <td><input type="text" name="User" size="15" required="required" /></td>
-                        </tr>
-                        <tr>
-                        <td>Wachtwoord:</td>
-                        <td><input type="password" name="Pass" size="15" required="required" /></td>
-                        </tr>
-                        <tr>
-                        <td>&nbsp;</td>
-                        <td><input type="submit" name="submit" value="Login" /></td>
-                        </tr>
-                        </table></form>
-                    <?php
-                }
-                ?>   
-<?php
-require('php/eind.php');
+include('db.php');
+session_start();
+
+    
+if (isset($_POST['Gebruikersnaam'])){
+    $Gebruikersnaam = $_POST['Gebruikersnaam'];
+    $Wachtwoord = $_POST['Wachtwoord'];
+    $hash = hash('sha384',$Wachtwoord);
+    $sql = "SELECT * FROM Gebruikers WHERE Gebruikersnaam ='".$Gebruikersnaam."' AND Wachtwoord = '".$hash."'";
+    $records= mysqli_query($DBverbinding, $sql);
+    if (mysqli_num_rows($records)== 1) {
+        $_SESSION["user"] = "$Gebruikersnaam";
+        $_SESSION["melding"] = "U bent ingelogd met {$SESSION["user"]} .";
+        $_SESSION["alert"] = 1;
+    }
+    else {
+        $_SESSION["melding"] = "Het is niet gelukt om in te loggen. Probeer het nog een keer.";
+        $_SESSION["alert"] = 0;
+    }
+header("Location:mijnProject/index.php");
+}
 ?>
+
+                        
--- public/mijnProject/php/inloggenformulier.php
+++ public/mijnProject/php/inloggenformulier.php
@@ -8,7 +8,7 @@ if ($_SESSION["user"]) {
 else {
 ?>
 
-                <form method="POST" class="form-inline" action="php/login.php">
+                <form method="POST" class="form-inline" action="php/Inloggem.php">
                     <input type="email" class="form-control mb-2 mr-sm-2" placeholder="e-mail" id="mail" name="mail">
                     <input type="password" class="form-control mb-2 mr-sm-2" placeholder="wachtwoord" id="pass" name="pass">
                     <button type="submit" class="btn btn-secondary mb-2">Login</button>
