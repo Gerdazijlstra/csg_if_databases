eba5a902ffbd541cb563e446099c9299bc7b3575
diannedejong1 <dianne.dejong@home.nl>
2020-06-11 10:16:27 +0000

    xdzs

--- public/mijnProject/recept1.php
+++ public/mijnProject/recept1.php
@@ -1,8 +1,23 @@
 <?php
 require('php/begin.php');
+?>
+<img src="pics/zandkoekje.jpg" alt="recept1" height="80%" style="margin: auto" >
+<h1>zandkoekjes </h1>
+<br>
+<h3>benodigheden</h3>
+<ul>
+<li>1 middelgroot ei</li>
+<li> 150 g ongezouten roomboter (koud) </li>
+<li> 100 g witte basterdsuiker </li>
+<li> 200 g tarwebloem </li>
+</ul>
+<h3> instructies </h3>
+<p> 1.	Splits het ei en gebruik alleen de eidooier. Meng vervolgens alle ingrediënten in de keukenmachine tot het deeg een korrelige structuur heeft. Kneed de deegkruimels met koele handen tot een samenhangend deeg. Rol tot een bal en verpak in vershoudfolie en laat het 30 min. in de koelkast rusten. </p>
+<br>
+<p>2.	Verwarm de oven voor op 175 °C. Bestuif het werkblad met wat bloem en rol het deeg uit tot een lap van ca. 40 x 40 cm en 3-4 mm dik. Steek met behulp van uitsteekvormpjes of een glas koekjes uit het deeg. Kneed het resterende deeg tot een bal en rol opnieuw uit tot al het deeg gebruikt is. Leg de koekjes op enige afstand van elkaar op een met bakpapier beklede bakplaat. </p>
+<br>
+<p>
 
- 
-
-
+<?php
 require('php/eind.php');
 ?>
