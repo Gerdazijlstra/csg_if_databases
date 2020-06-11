3f81ecaf79c321a821b507659575b8e738aca2fe
Gerdazijlstra <150154@csgaugustinus.nl>
2020-06-11 18:04:27 +0000

    frevfarw

--- public/mijnProject/index.php
+++ public/mijnProject/index.php
@@ -7,7 +7,7 @@ require('php/begin.php');
 require('db.php');
 ?>
                 <div class="container">
-                    <img src="achtergrond1.JPG" alt="bla" style="width:100%;">
+                    <img src="pics/achtergrond1.JPG" alt="bla" style="width:100%;">
 
                     <div class="centered">Welkom op Bakken & Co!</div>
                     <p class="center">Op deze bakwebsite kun je allemaal recepten vinden die je kunt uitproberen. Laat je inspireren door de lekkere baksels van Bakken & Co. Veel bakplezier!</p>
