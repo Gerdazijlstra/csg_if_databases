a3713cd372285a6b2f426145595f8c988654fefa
Gerdazijlstra <150154@csgaugustinus.nl>
2020-06-11 17:22:27 +0000

    cx

--- public/mijnProject/recept2.php
+++ public/mijnProject/recept2.php
@@ -1,5 +1,28 @@
 <?php
 require('php/begin.php');
+?>
+<div id= "lings">
+<h1>Appelcake </h1>
+<br>
+<h3>benodigheden</h3>
+<ul>
+<li>75 g ongezouten roomboter</li>
+<li>250 g zelfrijzend bakmeel </li>
+<li>2 appels   </li>
+<li>2 middelgrote eieren  </li>
+<li>100 g Canderel culinaire </li>
+<li>100 ml halfvolle melk </li>
+<li>1 el tarwebloem  </li>
+</ul>
+<h3> instructies </h3>
+<p> 1.	Roer de boter zacht. Zeef het bakmeel. Schil de appels en verwijder het klokhuis. Snijd de appels in 8 stukjes. Splits de eieren en klop de eiwitten stijf. </p>
 
+<p>2.	Mix in een grote kom de boter, Canderel en melk. Voeg de eidooiers toe en mix goed. Voeg beetje bij beetje het bakmeel toe en mix tot een glad beslag. Spatel de stijfgeklopte eiwitten erdoor.</p>
+<p>3.	Vet de cakevorm in en bestrooi met de bloem. Doe het deeg in de vorm en duw de stukjes appel in het deeg. Bak de cake ca. 50 min.</p>
+</div>
+<div id= "rechts">
+<img src="pics/applcake.png" alt="recept2" height="300px" style="margin: auto" >
+</div>
+<?php
 require('php/eind.php');
 ?>
