d81a1b66fb697505def089fd4815e4f545ecac02
diannedejong1 <dianne.dejong@home.nl>
2020-06-12 23:16:01 +0000

    n

--- public/mijnProject/recepten.php
+++ public/mijnProject/recepten.php
@@ -9,24 +9,24 @@ require('db.php');
                     <div class="een">
                         <img src="pics/Koekies.png" alt="recept1" height="80%" style="margin: auto" >
                         <br><br>
-                        <a href="recept/recept1.php">Zandkoekjes</a>
+                        <a href="recept1.php">Zandkoekjes</a>
                     </div>
                     <div class="twee">
                         <img src="pics/applcake.png" alt="recept2" height="80%" style="margin: auto" >  
                         <br><br>
-                        <a href="recept/recept2.php">Appelcake</a>                  
+                        <a href="recept2.php">Appelcake</a>                  
                     </div>
                 </div>
                 <div id="b">
                     <div class="drie">
                         <img src="pics/tompoes.jpg" alt="recept3" height="80%" style="margin: auto" >
                         <br><br>
-                        <a href="recept/recept3.php">Aardbeientompoes</a>
+                        <a href="recept3.php">Aardbeientompoes</a>
                     </div>
                     <div class="vier">
                         <img src="pics/chocotaart.png" alt="recept4" height="80%" style="margin: auto" >
                         <br><br>
-                        <a href="recept/recept4.php">Chocoladetaart</a>
+                        <a href="recept4.php">Chocoladetaart</a>
                     </div>
                 </div> 
                 <h1>Hartige recepten</h1>
@@ -35,24 +35,24 @@ require('db.php');
                     <div class="vijf">
                         <img src="pics/broccoli.png" alt="recept5" height="80%" style="margin: auto" >
                         <br><br>
-                        <a href="recept/recept5.php">Broccoliquiche</a>
+                        <a href="recept5.php">Broccoliquiche</a>
                     </div>
                     <div class="zes">
                         <img src="pics/nacho.png" alt="recept6" height="80%" style="margin: auto" >
                         <br><br>
-                        <a href="recept/recept6.php">Nachos</a>
+                        <a href="recept6.php">Nachos</a>
                     </div>
                 </div>
                 <div id="d">
                     <div class="zeven">
                         <img src="pics/broodtaart.png" alt="recept7" height="80%" style="margin: auto" >
                         <br><br>
-                        <a href="recept/recept7.php">Hartige broodtaartjes</a>
+                        <a href="recept7.php">Hartige broodtaartjes</a>
                     </div>
                     <div class="acht">
                         <img src="pics/hartigtaart.png" alt="recept8" height="80%" style="margin: auto" >
                         <br><br>
-                        <a href="recept/recept8.php">Hartige taart met tonijn</a>
+                        <a href="recept8.php">Hartige taart met tonijn</a>
                     </div>
                     </div>
 <?php
