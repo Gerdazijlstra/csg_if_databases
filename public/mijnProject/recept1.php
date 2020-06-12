eba16dbb26f26cb23f13caf41671b4efd83ffb1b
Gerdazijlstra <150154@csgaugustinus.nl>
2020-06-12 10:19:01 +0000

    fdxgftx

--- public/mijnProject/recept1.php
+++ public/mijnProject/recept1.php
@@ -2,7 +2,7 @@
 require('php/begin.php');
 ?>
 <div id= "lings">
-<h1>zandkoekjes </h1>
+<img src="pics/Koekies.png" alt="recept1" width="90%" >
 <br>
 <h3>benodigheden</h3>
 <ul>
@@ -11,14 +11,14 @@ require('php/begin.php');
 <li> 100 g witte basterdsuiker </li>
 <li> 200 g tarwebloem </li>
 </ul>
+</div>
+<div id= "rechts">
+<h1>zandkoekjes </h1>
 <h3> instructies </h3>
 <p> 1.	Splits het ei en gebruik alleen de eidooier. Meng vervolgens alle ingrediënten in de keukenmachine tot het deeg een korrelige structuur heeft. Kneed de deegkruimels met koele handen tot een samenhangend deeg. Rol tot een bal en verpak in vershoudfolie en laat het 30 min. in de koelkast rusten. </p>
 
 <p>2.	Verwarm de oven voor op 175 °C. Bestuif het werkblad met wat bloem en rol het deeg uit tot een lap van ca. 40 x 40 cm en 3-4 mm dik. Steek met behulp van uitsteekvormpjes of een glas koekjes uit het deeg. Kneed het resterende deeg tot een bal en rol opnieuw uit tot al het deeg gebruikt is. Leg de koekjes op enige afstand van elkaar op een met bakpapier beklede bakplaat. </p>
 <p>3.	Bak de koekjes in het midden van de oven in 15-20 min. lichtbruin en gaar. Neem ze van de bakplaat en laat afkoelen, bij voorkeur op een rooster.</p>
-</div>
-<div id= "rechts">
-<img src="pics/Koekies.png" alt="recept1" height="300px" style="margin: auto" >
 </div>
 <?php
 require('php/eind.php');
