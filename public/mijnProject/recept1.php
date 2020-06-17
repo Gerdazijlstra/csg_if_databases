2f72ee056cf82f0ebca7210371db35b1349a2d68
diannedejong1 <dianne.dejong@home.nl>
2020-06-17 16:56:51 +0000

    vvdf

--- public/mijnProject/recept1.php
+++ public/mijnProject/recept1.php
@@ -1,6 +1,9 @@
 <?php
 require('php/begin.php');
 require('php/databese.php');
+$nummer = 1;
+$recept = "SELECT receptTitel  FROM Recept WHERE receptId='".$nummer."'";
+echo $recept;
 ?>
 <div id="boven">
     <h1>zandkoekjes </h1>
