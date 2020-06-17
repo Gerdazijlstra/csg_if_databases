fce55a5044593299f6b8c6e1cc5e1525121781e7
Diannedejong <150512@csgaugustinus.nl>
2020-06-17 11:27:25 +0000

    ,m ,

--- public/mijnProject/inloggen1.php
+++ public/mijnProject/inloggen1.php
@@ -8,28 +8,44 @@ $mysqli = new mysqli($localhost, $Gebruikersnaam, $Wachtwoord, $Bakkenenco);
 
     
 require('php/begin.php');
+session_start();
+
     $naam = $_GET["gebruikersnaam"];
     $woord = $_GET["wachtwoord"];
+    $sql = "SELECT * FROM Gebruikers WHERE Gebruikersnaam='".$gebr."' AND Wachtwoord='".$pass."'";
+    $records = mysqli_query($DBverbinding, $sql);
     $ingelogd = 0 ;
-
-    if (empty($naam) && empty($woord)) {
-        echo 'log in';
-        }
-        else {
-            if ($naam == $Gebruikersnaam && $woord == $Wachtwoord) {
-                echo 'u bent ingelogd';
-                $ingelogd = 1;
-            } else {
-                echo 'uw gebruikersnaam of wachtwoord is niet goed, probeer het nog eens';
-            }
-        }
-
+    if ($naam == $gebr && $woord == $pass){
+        echo 'u bent ingelogd';
+        $ingelogd = 1;
+    } 
+    else {
+        echo 'uw gebruikersnaam of wachtwoord is niet goed, probeer het nog eens';
+    }
     if ($ingelogd== '1') {
-        header("Location: ../mijnProject/index.php");
-}
-else {
+        
+    }
+    else {
+        ?>
+        <form action="inloggen1.php" method="post">
+            gebruikersnaam: <input type="text" name="gebruikersnaam">
+            <br>
+            wachtwoord: <input type="password" name="wachtwoord">
+            <br>
+            <input type="submit" value="inloggen    " >
+        </form>
+        <br>
+        <?php
+    }
+
+
+
+
+
+
 ?>
-<form action="inloggen1.php" method="get">
+
+<form action="inloggen1.php" method="post">
     gebruikersnaam: <input type="text" name="gebruikersnaam">
     <br>
     wachtwoord: <input type="password" name="wachtwoord">
@@ -39,6 +55,5 @@ else {
 <br>
 
 <?php
-}
     require('php/eind.php');
 ?>
--- public/mijnProject/vnr/index.php
+++ public/mijnProject/vnr/index.php
@@ -12,7 +12,7 @@ echo $_SESSION["melding"];
 
 <h1>Formulier</h1>
     <form method="POST" class="form-inline" action="login.php">
-        <input type="email" class="form-control mb-2 mr-sm-2" placeholder="e-mail" id="mail" name="mail">
+        <input type="text" class="form-control mb-2 mr-sm-2" placeholder="gebruikersnaam" id="naam" name="naam">
         <input type="password" class="form-control mb-2 mr-sm-2" placeholder="wachtwoord" id="pass" name="pass">
         <button type="submit" class="btn btn-secondary mb-2">Login</button>
     </form>
