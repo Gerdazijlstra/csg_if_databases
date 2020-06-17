af8dff3136ac6cba1065d438ecbb87a764be51e4
Diannedejong <150512@csgaugustinus.nl>
2020-06-17 14:11:53 +0000

    n;on;

--- public/mijnProject/inloggen1.php
+++ public/mijnProject/inloggen1.php
@@ -1,11 +1,5 @@
 <?php
-$servernaam = "localhost";
-$Gebruikersnaam = 'username';
-$Wachtwoord = 'password';
-$database = 'Bakkenenco';
-$DBverbinding = mysqli_connect($servernaam, $Gebruikersnaam, $Wachtwoord, $database);
-$mysqli = new mysqli($localhost, $Gebruikersnaam, $Wachtwoord, $Bakkenenco);
-
+require('php/databese.php');
 session_start();
 if (isset($_POST['naam'])) {
     $naam = $_POST['naam'];
--- public/mijnProject/php/reacties.php
+++ public/mijnProject/php/reacties.php
@@ -1,3 +1,15 @@
+<?php
+session_start();
+require('php/databese.php');
+$ingelogd = $_SESSION["log"];
+if ($ingelogd == 1) {
+?>
+
+
 <div id="reactie">
 <h3>reacties</h3>
+
 </div>
+<?php
+}
+?>
