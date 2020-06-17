11b8cc2e708fb4f1f8620045dbe62621d085e09b
Gerdazijlstra <150154@csgaugustinus.nl>
2020-06-17 14:56:45 +0000

    dwdeqf

--- public/mijnProject/uitlog.php
+++ public/mijnProject/uitlog.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 require('php/begin.php');
-echo $_SESSION["melding"];
+echo "<a>".$_SESSION["melding"]. "</a>";
 ?>
 <br><br>
 <a href="logout.php">klik hier om uit te loggen</a>
